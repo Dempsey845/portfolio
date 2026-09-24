61bd2925b2e8d4b575ffcd053784367a360fc2c5
Dempsey845 <dempseynewton1@gmail.com>
2026-09-24 23:09:21 +0100

    Made the nav options functional.

--- scripts/header.js
+++ scripts/header.js
@@ -1,20 +1,27 @@
 const header = document.getElementById("header");
 
+const isHomePage =
+  window.location.pathname.endsWith("index.html") ||
+  window.location.pathname.endsWith("/");
+
 header.innerHTML = `
-    <div class="left-section">
-        <div id="main-nav-option" class="nav-option">
-          <a>Dempsey Newton</a>
-        </div>
-      </div>
-      <div class="right-section">
-        <div class="nav-option">
-          <a>HOME</a>
-        </div>
-        <div class="nav-option">
-          <a>ABOUT</a>
-        </div>
-        <div class="nav-option">
-          <a>GAMES</a>
-        </div>
-      </div>
+  <div class="left-section">
+    <div id="main-nav-option" class="nav-option">
+      <a href="index.html">Dempsey Newton</a>
+    </div>
+  </div>
+
+  <div class="right-section">
+    <div class="nav-option">
+      <a href="index.html">HOME</a>
+    </div>
+
+    <div class="nav-option">
+      <a href="${isHomePage ? "#about-me" : "index.html#about-me"}">ABOUT</a>
+    </div>
+
+    <div class="nav-option">
+      <a href="games.html">GAMES</a>
+    </div>
+  </div>
 `;
